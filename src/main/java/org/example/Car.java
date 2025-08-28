7aea750eadfb02919bad3fc840aea6ae6c5e0826
KroegerLeif <Leif.kr@outlook.com>
2025-08-28 12:05:17 +0200

    Complition of Car Task

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -8,14 +8,22 @@ public class Car {
     private String model;
     private String color;
     private int year;
+    private int maxSpeed;
     private int speed;
+    private int distance;
+    private boolean isRunning;
 
-    public Car(String brand, String model, String color, int year, int speed) {
+    public Car(String brand, String model, String color, int year, int maxSpeed) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.year = year;
-        this.speed = speed;
+        this.maxSpeed = speed;
+
+        //Standard values
+        this.speed = 0;
+        this.distance = 0;
+        this.isRunning = false;
     }
 
     public void startCar() {
@@ -26,6 +34,21 @@ public class Car {
         System.out.println("Car stopped");
     }
 
+    public void accelerate(int acceleration) {
+        speed += acceleration;
+    }
+
+    public void accelerate() {
+        accelerate(10);
+    }
+
+    public void decelerate(int deceleration) {
+        speed -= deceleration;
+    }
+
+    public void decelerate() {
+        decelerate(10);
+    }
 
     //SETTER
 
@@ -45,9 +68,22 @@ public class Car {
         this.year = year;
     }
 
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
     public void setSpeed(int speed) {
         this.speed = speed;
     }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
 
     //GETTER
 
@@ -67,19 +103,31 @@ public class Car {
         return year;
     }
 
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
     public int getSpeed() {
         return speed;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public boolean getIsRunning() {
+        return isRunning;
     }
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Car car)) return false;
-        return year == car.year && speed == car.speed && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color);
+        return year == car.year && maxSpeed == car.maxSpeed && speed == car.speed && distance == car.distance && isRunning == car.isRunning && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(brand, model, color, year, speed);
+        return Objects.hash(brand, model, color, year, maxSpeed, speed, distance, isRunning);
     }
 
     @Override
@@ -89,7 +137,10 @@ public class Car {
                 ", model='" + model + '\'' +
                 ", color='" + color + '\'' +
                 ", year=" + year +
+                ", maxSpeed=" + maxSpeed +
                 ", speed=" + speed +
+                ", distance=" + distance +
+                ", isRunning=" + isRunning +
                 '}';
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,18 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //First Car
+        Car car = new Car("BMW", "M3", "Blue", 2019, 100);
+        car.startCar();
+        car.stopCar();
+        System.out.println(car);
+
+        //Second Car
+        Car car2 = new Car("Audi", "A4", "Red", 2018, 120);
+        car2.startCar();
+        car2.accelerate(70);
+        System.out.println(car2.getSpeed());
+        System.out.println(car2);
     }
 }
